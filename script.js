7d0a730eafce5595899237f2b0476975aa37d2e7
Leandro <leandrohjesus@hotmail.com>
2023-03-21 08:16:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,26 @@ function typeWriter(p) {
     const textoArray = p.innerHTML.split('')
     p.innerHTML = ''
     textoArray.forEach((letra, i) => {
-           setTimeout(() => p.innerHTML += letra, 60 * i)});
+        setTimeout(() => p.innerHTML += letra, 60 * i)});
 }
+
+const btnMobile = document.querySelector('.btn-mobile');
+    
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefaut()
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    }
+
+    else{
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}       
+    
+    btnMobile.addEventListener('click', toggleMenu);
+    btnMobile.addEventListener('touchstart', toggleMenu);
